88c4831b34ae7910a94afe6940b87cb3e1ba5995
jeffacosta <killerbe1000@gmail.com>
2021-07-08 14:22:03 -0600

    pull

---
+++ tests/Unit/PullTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class PullTest extends TestCase
+{
+    /**
+     * Confirmar los pull request
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $this->assertTrue(true);
+    }
+}
